57e67ae29739fb8122cf9cfbe1ac5536c8efb317
pusinskas <pedrousinskas@smartenup.com.br>
2016-04-26 01:52:49 -0300

    Add to control version

---
+++ src/Nop.Plugin.DiscountRules.HasSpentAmount/Controllers/DiscountRulesHasSpentAmountController.cs
@@ -0,0 +1,103 @@
+﻿using Nop.Core;
+using Nop.Core.Domain.Discounts;
+using Nop.Plugin.DiscountRules.HasSpentAmount.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Discounts;
+using Nop.Services.Security;
+using Nop.Web.Framework.Controllers;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Nop.Plugin.DiscountRules.HasSpentAmount.Controllers
+{
+    [AdminAuthorize]
+    public class DiscountRulesHasSpentAmountController : BasePluginController
+    {
+        private readonly IDiscountService _discountService;
+        private readonly ISettingService _settingService;
+        private readonly IPermissionService _permissionService;
+
+        public DiscountRulesHasSpentAmountController(IDiscountService discountService,
+            ISettingService settingService, 
+            IPermissionService permissionService)
+        {
+            this._discountService = discountService;
+            this._settingService = settingService;
+            this._permissionService = permissionService;
+        }
+
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+            //little hack here
+            //always set culture to 'en-US' (Telerik has a bug related to editing decimal values in other cultures). Like currently it's done for admin area in Global.asax.cs
+            CommonHelper.SetTelerikCulture();
+
+            base.Initialize(requestContext);
+        }
+
+        public ActionResult Configure(int discountId, int? discountRequirementId)
+        {
+            if (!_permissionService.Authorize(StandardPermissionProvider.ManageDiscounts))
+                return Content("Access denied");
+
+            var discount = _discountService.GetDiscountById(discountId);
+            if (discount == null)
+                throw new ArgumentException("Discount could not be loaded");
+
+            if (discountRequirementId.HasValue)
+            {
+                var discountRequirement = discount.DiscountRequirements.FirstOrDefault(dr => dr.Id == discountRequirementId.Value);
+                if (discountRequirement == null)
+                    return Content("Failed to load requirement.");
+            }
+
+            var spentAmountRequirement = _settingService.GetSettingByKey<decimal>(string.Format("DiscountRequirement.HasSpentAmount-{0}", discountRequirementId.HasValue ? discountRequirementId.Value : 0));
+
+            var model = new RequirementModel();
+            model.RequirementId = discountRequirementId.HasValue ? discountRequirementId.Value : 0;
+            model.DiscountId = discountId;
+            model.SpentAmount = spentAmountRequirement;
+
+            //add a prefix
+            ViewData.TemplateInfo.HtmlFieldPrefix = string.Format("DiscountRulesHasSpentAmount{0}", discountRequirementId.HasValue ? discountRequirementId.Value.ToString() : "0");
+
+            return View("~/Plugins/DiscountRules.HasSpentAmount/Views/DiscountRulesHasSpentAmount/Configure.cshtml", model);
+        }
+
+        [HttpPost]
+        public ActionResult Configure(int discountId, int? discountRequirementId, decimal spentAmount)
+        {
+            if (!_permissionService.Authorize(StandardPermissionProvider.ManageDiscounts))
+                return Content("Access denied");
+
+            var discount = _discountService.GetDiscountById(discountId);
+            if (discount == null)
+                throw new ArgumentException("Discount could not be loaded");
+
+            DiscountRequirement discountRequirement = null;
+            if (discountRequirementId.HasValue)
+                discountRequirement = discount.DiscountRequirements.FirstOrDefault(dr => dr.Id == discountRequirementId.Value);
+
+            if (discountRequirement != null)
+            {
+                //update existing rule
+                _settingService.SetSetting(string.Format("DiscountRequirement.HasSpentAmount-{0}", discountRequirement.Id), spentAmount);
+            }
+            else
+            {
+                //save new rule
+                discountRequirement = new DiscountRequirement
+                {
+                    DiscountRequirementRuleSystemName = "DiscountRequirement.HasSpentAmount"
+                };
+                discount.DiscountRequirements.Add(discountRequirement);
+                _discountService.UpdateDiscount(discount);
+                
+                _settingService.SetSetting(string.Format("DiscountRequirement.HasSpentAmount-{0}", discountRequirement.Id), spentAmount);
+            }
+            return Json(new { Result = true, NewRequirementId = discountRequirement.Id }, JsonRequestBehavior.AllowGet);
+        }
+        
+    }
+}
---
+++ src/Nop.Plugin.DiscountRules.HasSpentAmount/RouteProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Nop.Web.Framework.Mvc.Routes;
+
+namespace Nop.Plugin.DiscountRules.HasSpentAmount
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("Plugin.DiscountRules.HasSpentAmount.Configure",
+                 "Plugins/DiscountRulesHasSpentAmount/Configure",
+                 new { controller = "DiscountRulesHasSpentAmount", action = "Configure" },
+                 new[] { "Nop.Plugin.DiscountRules.HasSpentAmount.Controllers" }
+            );
+        }
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
